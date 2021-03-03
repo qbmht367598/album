be90fc7f65dea552a01be696f7acf64a1f4ce680
waki akina <qbmht367598@gmail.com>
2021-03-03 21:24:14 +0900

    ページトップボタン設置

---
+++ js/jquery.js
@@ -0,0 +1,10 @@
+ $(function(){
+   
+    /*topページボタン*/
+    $('.page-top').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+ });
